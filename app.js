9599395b4bc225d4f10ae853444fee7589e341b6
Ethan Anderson <ethanandy90@gmail.com>
2024-05-13 12:10:56 -0600

    get around CORS

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ const app = express();
 
 const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
-const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.g3viuar.mongodb.net/places?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.g3viuar.mongodb.net/mern?retryWrites=true&w=majority&appName=Cluster0`;
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
